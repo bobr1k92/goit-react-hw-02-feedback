d794d874aaa3081342ed870a7eb85882aedc6749
Denis Mazurov <19bobrik92@gmail.com>
2023-02-07 03:31:12 +0200

    +

--- src/components/FeedBack/Controls.jsx
+++ src/components/FeedBack/Controls.jsx
@@ -1,5 +1,5 @@
-import PropTypes from 'prop-types';
-import { Component } from 'react';
+// import PropTypes from 'prop-types';
+// import { Component } from 'react';
 
 const Controls = ({ onIncrementGood, onIncrementNeutral, onIncrementBad }) => (
 
