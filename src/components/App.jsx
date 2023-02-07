f6efbcc2e2ec0af0776a1f44e78cb418e5f610ea
Denis Mazurov <19bobrik92@gmail.com>
2023-02-07 03:09:08 +0200

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import FeedBackCounter from './Feetback/Feedback'
+
 export const App = () => {
   return (
     <div
@@ -10,7 +12,8 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      
+      <FeedBackCounter/>
     </div>
   );
 };
