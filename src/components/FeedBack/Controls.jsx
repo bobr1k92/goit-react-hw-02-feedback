20fd6afe94b50c1adbb2aee910ea03d59825c3cb
Denis Mazurov <19bobrik92@gmail.com>
2023-02-07 03:06:43 +0200

    +

---
+++ src/components/FeedBack/Controls.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import { Component } from 'react';
+
+const Controls = ({ onIncrementGood, onIncrementNeutral, onIncrementBad }) => (
+
+    <div>
+        <h2>Please leave feedback</h2>
+        <div>
+            <button type='button' onClick={onIncrementGood}>Good</button>
+            <button type='button' onClick={onIncrementNeutral}>Neutral</button>
+            <button type='button' onClick={onIncrementBad}>Bad</button>
+        </div>
+    </div>
+);
+
+
+export default Controls;
