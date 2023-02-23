27c322e34547dc536e9438fceebc6493290c1bb3
Denis Mazurov <19bobrik92@gmail.com>
2023-02-23 03:58:24 +0200

    +

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,14 +1,14 @@
-// import { BsEmojiSmile, BsEmojiFrown, BsEmojiNeutral, BsEmojiLaughing, BsPercent } from "react-icons/bs";
+// import { BsEmojiSmile, BsEmojiNeutral, BsEmojiFrown, BsPercent, BsEmojiLaughing } from "react-icons/bs";
 import css from "./statistics.module.css"
 
 const Statistics = ({good, neutral, bad, total, percent}) => (
 
 <ul className={css.statisticsList}>
-      <li> Good: {good}</li>
-      <li> Neutral: {neutral}</li>
-      <li> Bad: {bad}</li>
-      <li> Total: {total}</li>
-      <li> PositivePercentage: {percent}%</li>
+      <li className={css.statisticsItem}><span className={css.emodji}><BsEmojiSmile size={20}/></span> Good: {good}</li>
+      <li className={css.statisticsItem}><span className={css.emodji}><BsEmojiNeutral size={20}/></span> Neutral: {neutral}</li>
+      <li className={css.statisticsItem}><span className={css.emodji}><BsEmojiFrown size={20}/></span> Bad: {bad}</li>
+      <li className={css.statisticsItem}><span className={css.emodji}><BsPercent size={20}/></span> Total: {total}</li>
+      <li className={css.statisticsItem}><span className={css.emodji}><BsEmojiLaughing size={20}/></span> PositivePercentage: {percent}%</li>
 </ul>
 );
 
